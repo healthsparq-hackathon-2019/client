c2a4a6646883499196f895ce79166464e067dca3
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-01 17:16:39 -0700

    landing page start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Landing from './components/landing/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Healthsparq Hackathon</h1>
+        <Landing/>
       </div>
     );
   }

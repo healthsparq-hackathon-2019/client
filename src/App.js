922409860442af4492c669eb23a9813cc1f6a7c6
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-04-04 11:18:32 -0700

    added doctors pic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Landing from './components/landing/Landing';
+// import Landing from './components/landing/Landing';
+import Details from './components/details/Details';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Landing/>
-      </div>
+      <Details />
     );
   }
 }

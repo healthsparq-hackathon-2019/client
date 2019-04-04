e9f20324bed94952e32e832a1ac5f153044d487f
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-04 15:37:46 -0700

    date styling

--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -11,13 +11,13 @@ export default function Appointment(){
                         <img className={styles.hospitalPicture} src='http://media.bizj.us/view/img/6038891/maintower-newfacade-rendering*1200xx732-412-0-69.jpg' alt='St. Vincent Hospital'/>
                     </div>
                 </div>
-                
                 <div className={styles.rightColumn}>
                     <div className={styles.buttonsContainer}>
                         <Link to='/details' className={styles.button}>DETAILS</Link>
                         <Link tp='details' className={styles.button}>TRANSPORT</Link>
                     </div>
                 </div>
+               <h3 className={styles.date}>Tomorrow</h3>
             </div>
         </>
     )

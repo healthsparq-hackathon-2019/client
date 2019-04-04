7330cb07eed8f90c49c53340fd4260d41a8a993a
Huy Yang <yanhshuy@pdx.edu>
2019-04-04 12:58:18 -0700

    Formatted with CSS

--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -3,6 +3,21 @@ import styles from './Appointment.module.css';
 
 export default function Appointment(){
     return(
-        <h1>Hello</h1>
+        <>
+            <div className={styles.box}>
+                <div className={styles.leftColumn}>
+                    <div className={styles.hospitalPictureContainer}>
+                        <img className={styles.hospitalPicture} src='http://media.bizj.us/view/img/6038891/maintower-newfacade-rendering*1200xx732-412-0-69.jpg' alt='St. Vincent Hospital'/>
+                    </div>
+                </div>
+                
+                <div className={styles.rightColumn}>
+                    <div className={styles.buttonsContainer}>
+                        <button className={styles.button}>Details</button>
+                        <button className={styles.button}>Transport</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Landing.module.css';
-import { FaHandPointUp } from 'react-icons/fa';
 import Appointment from '../appointment/Appointment';
 
 export default function Landing() {
@@ -9,11 +8,6 @@ export default function Landing() {
         <header>
             <h3 className={styles.h3}>HealthSparq Hackathon</h3>
         </header>
-        <h2 className={styles.h2}>Plan Your Next Appointment</h2>
-        <div className={styles.startContainer}>
-            <h4 className={styles.h4}>Tap to start</h4>
-            <FaHandPointUp className={styles.finger}/>
-        </div>
         <Appointment/>
     </div>
   )

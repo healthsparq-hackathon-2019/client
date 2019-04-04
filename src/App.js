cb9994df459e5ce215fa98efbc3bc515b4b2516f
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-04-04 14:56:28 -0700

    added more details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 // import Landing from './components/landing/Landing';
 import Details from './components/details/Details';
-
+// import About from './components/about/About';
 class App extends Component {
   render() {
     return (

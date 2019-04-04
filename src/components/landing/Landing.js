e1499cd24ffbd83d3823fdc8db4ac2c005a7d221
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-04 15:28:07 -0700

    background colors

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -8,10 +8,18 @@ export default function Landing() {
   return(
     <div className={styles.background}>
         <Header/>
-        <Appointment/>
-        <Appointment/>
-        <Appointment/>
-        <Appointment/>
+        <div className={styles.dark}>
+          <Appointment/>
+          <Appointment/>
+        </div>
+        <div className={styles.light}>
+          <Appointment/>
+          <Appointment/>
+        </div>
+        <div className={styles.dark}>
+          <Appointment/>
+          <Appointment/>
+        </div>
         <Footer/>
     </div>
   )

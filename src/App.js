e994c9aebe3bba45d7df38e29db2980f2fca89cc
Huy Yang <yanhshuy@pdx.edu>
2019-04-04 14:45:18 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 // import Landing from './components/landing/Landing';
 import Details from './components/details/Details';
+import Footer from './components/footer/Footer';
 
 class App extends Component {
   render() {
     return (
-      <Details />
+      <>
+        <Details />
+        <Footer />
+      </>
     );
   }
 }

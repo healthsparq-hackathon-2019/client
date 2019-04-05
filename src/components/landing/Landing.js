28e75d4640c85cf9d6da6747548430061a1d8c84
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-05 09:20:46 -0700

    refactor into class

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,16 +4,19 @@ import Appointment from '../appointment/Appointment';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
 
+const data = [
+  {
+    title: 'Check up',
+    date: 'Tomorrow',
+    img: 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Fhealth-care-8%2F512%2Fdoctor_appointment-512.png&f=1'
+  }
+]
+
 export default function Landing() {
   return(
     <div className={styles.background}>
         <Header/>
-          <Appointment/>
-          <Appointment/>
-          <Appointment/>
-          <Appointment/>
-          <Appointment/>
-          <Appointment/>
+          <Appointment appointment={data[0]}/>
         <Footer/>
     </div>
   )

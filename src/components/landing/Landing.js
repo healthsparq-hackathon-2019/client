7eb7a87c6db5e17a8ea206282036281d95b50b3c
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-05 09:31:38 -0700

    done

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -9,6 +9,16 @@ const data = [
     title: 'Check up',
     date: 'Tomorrow',
     img: 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Fhealth-care-8%2F512%2Fdoctor_appointment-512.png&f=1'
+  },
+  {
+    title: 'Immunization',
+    date: 'Thursday',
+    img: 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.iconfinder.com%2Fdata%2Ficons%2Fhealth-fitness-icons%2F60%2FneedleColor-512.png&f=1'
+  },
+  {
+    title: 'New Cast',
+    date: '18th',
+    img: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FncB%2FGbA%2FncBGbA87i.gif&f=1'
   }
 ]
 
@@ -17,6 +27,8 @@ export default function Landing() {
     <div className={styles.background}>
         <Header/>
           <Appointment appointment={data[0]}/>
+          <Appointment appointment={data[1]}/>
+          <Appointment appointment={data[2]}/>
         <Footer/>
     </div>
   )

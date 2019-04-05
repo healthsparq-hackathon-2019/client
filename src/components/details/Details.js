550814032e76f6e2108510f1e74e17dcc14986dc
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-04-05 08:43:37 -0700

    mostly finished mobile version.

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import pic from '../../assets/docpic.jpg';
 import styles from './Details.module.css';
-import { FaHome } from 'react-icons/fa';
+import { FaCalendarPlus, FaTimes } from 'react-icons/fa';
 
 function Details() {
   return (
     <section className={styles.Details}>
-      <button className={styles.button}><FaHome /></button>
       <h2 className={styles.apt}>APPOINTMENT DETAILS</h2>
+      <section className={styles.visit}>
+        <h3>OFFICE VISIT</h3>
+        <p className={styles.p}>Friday, April 5</p>
+      </section>
+      <section className={styles.top}>
+        <div className={styles.div}>
+          <p className={styles.arrive}>Arrive by 4:45pm</p>
+          <p className={styles.start}>Appointment starts at 5pm</p>
+        </div>
+        <div className={styles.aptButton}>
+          <button className={styles.button}>add to my calendar<FaCalendarPlus /></button>
+          <button className={styles.button}>cancel apt<FaTimes /></button>
+        </div>
+      </section>
       <section className={styles.main}>
         <section className={styles.section}>
-        <h4 className={styles.h4}>
-          YOUR APPOINTMENT IS WITH
+          <h4 className={styles.h4}>
+            YOUR APPOINTMENT IS WITH
         </h4>
           <figure className={styles.figure}>
             <img src={pic} alt="doctor pic" className={styles.pic} />
             <figcaption className={styles.figcaption}> Melissa S. Yamauchi, M.D.</figcaption>
           </figure>
-        </section>
-        <section className={styles.section}>
           <p className={styles.atOn}>AT</p>
           <p className={styles.p}>Providence Hospital St. Vincent</p>
           <p className={styles.p}>9205 SW Barnes Rd.</p>
@@ -26,9 +37,11 @@ function Details() {
           <p className={styles.p}>Portland, OR 97225</p>
         </section>
         <section className={styles.section}>
-          <p className={styles.atOn}>ON</p>
-          <p className={styles.p}>April 5, 2019 at 5pm</p>
-          <p className={styles.p}>Check-in at 4:45pm</p>
+          <h4 className={styles.h4}>SET REMINDERS</h4>
+          <section className={styles.reminders}>
+          <button>PHONE</button>
+          <button>TEXT</button>
+          </section>
         </section>
       </section>
     </section>
